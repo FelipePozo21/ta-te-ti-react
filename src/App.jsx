c47acd7df2ee4f70593633219c076466aba7aea0
Felipe <felipepozo.dev@gmail.com>
2024-07-31 07:14:10 -0300

    check winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
+const TURN = {
+  X: 'x',
+  O: 'o'
+}
+
+const Square = ({children, uploadBoard, index, isSelected}) => {
+  const className = isSelected ? 'square is-selected' : 'square'
+  
+  return (
+    <div onClick={() => uploadBoard(index)} className={className}>
+      {children}
+    </div>
+  )
+}
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURN.X)
+  
+  const uploadBoard = (index) => {
+    checkWinner()
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const nextTurn = turn === TURN.X ? TURN.O : TURN.X
+
+    setTurn(nextTurn)
+  }
+
+  const checkWinner = (index) => {
+    const [a,b,c] = WINNER_COMBOS
+    
+    for(const check of WINNER_COMBOS) {
+      if(check[a] === board[index] &&
+        check[a] === check[b] &&
+        check[b] === check[c] 
+      ) {
+        console.log('win')
+      }
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="board">
+      <h1>Ta Te Ti</h1>
+      <section className="game">
+        {board.map((square, index) => (
+          <Square 
+            key={index}
+            index={index}
+            uploadBoard={uploadBoard}>
+              {square}
+          </Square>
+        ))}
+      </section>
+      <section className="turn">
+       <Square isSelected={turn === TURN.X ? true : false}>{TURN.X}</Square>
+       <Square isSelected={turn === TURN.O ? true : false}>{TURN.O}</Square>
+      </section>
+    </main>
   )
 }
 
